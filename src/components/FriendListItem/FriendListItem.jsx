9f9f867b71f54030941e2dfc045831f3fcd0bca6
Kseniia Korniichuk <kornksen010784@gmail.com>
2025-01-11 17:03:02 +0200

    changes styles status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,11 +4,11 @@ import styles from './FriendListItem.module.css'
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={styles.friendItem}>
-      <img src={avatar} alt="Avatar" className={styles.avatar} />
+      <img src={avatar} alt="Avatar" width="48" className={styles.avatar} />
       <p className={styles.name}>{name}</p>
-      <p className={isOnline ? 'status online' : 'status offline'}>
-        {isOnline ? 'Online' : 'Offline'}
-      </p>
+      <p className={`${styles.status} ${isOnline ? styles.online : styles.offline}`}>
+  {isOnline ? 'Online' : 'Offline'}
+</p>
     </div>
   );
 };
